fb8f7a90962bcff7ce0e5e7da034762e605b5eb3
honghao.shan <honghao.shan@dianping.com>
2014-06-02 19:35:20 +0800

    feature add layout for main

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// block IE (versions before 9.0)
+{
+    var userAgent = navigator.userAgent.toLowerCase();
+    var browser = {
+        version: (userAgent.match(/(?:firefox|opera|safari|chrome|msie)[\/: ]([\d.]+)/))[1],
+        safari: /version.+safari/.test(userAgent),
+        chrome: /chrome/.test(userAgent),
+        firefox: /firefox/.test(userAgent),
+        ie: /msie/.test(userAgent),
+        opera: /opera/.test(userAgent)
+    }
+    if (browser.ie && browser.version < 9) {
+        alert("Sorry, but this site appears to be too cool for your web browser(>_<)\nPlease use one of these recommended browsers instead:\n\nGoogle Chrome\nApple Safari\nMozilla Firefox\nMicrosoft Internet Explorer 9.0 or later");
+        window.location = "http://www.google.com/chrome";
+    }
+}
